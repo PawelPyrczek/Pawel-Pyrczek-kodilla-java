ec58f17de3b5191cf8b1da09055db6aac6ce7962
Paweł <pyrczekpawel@gmail.com>
2023-03-06 19:20:49 +0100

    test

---
+++ src/main/java/com/kodilla/kodillacourse/Main.java
@@ -0,0 +1,11 @@
+package com.kodilla.kodillacourse;
+
+import java.io.*;
+public class Main {
+
+
+    public static void main(String[] args) throws java.lang.Exception {
+        System.out.println("Hello world");
+    }
+}
+
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.kodilla.kodillacourse {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.kodilla.kodillacourse to javafx.fxml;
-    exports com.kodilla.kodillacourse;
-}
